1725347865ad04248bc1b2dea9fe0fb3d927f383
manyvarsath <avatar@LAPTOP-ACGOV0AD.localdomain>
2025-07-06 20:04:36 +0100

    Fixed 2-lazy_paginate.py

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -1,6 +1,14 @@
 import mysql.connector
 import time
 
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'varsath',
+    'password': 'varsath',
+    'port': 3306
+}
+DB_NAME = 'ALX_prodev'
+
 def paginate_users(connection, page_size, offset):
     if not connection:
         return []
@@ -8,7 +16,7 @@ def paginate_users(connection, page_size, offset):
     users_page = []
     try:
         cursor = connection.cursor(dictionary=True)
-        query = "SELECT user_id, name, email, age FROM user_data LIMIT %s OFFSET %s"
+        query = "SELECT * FROM user_data LIMIT %s OFFSET %s"
         cursor.execute(query, (page_size, offset))
         users_page = cursor.fetchall()
         cursor.close()
@@ -17,8 +25,8 @@ def paginate_users(connection, page_size, offset):
     
     return users_page
 
-def lazy_paginate(connection, page_size):
-    offset = 0
+def lazy_paginate(page_size, offset=0):
+    connection = mysql.connector.connect(**DB_CONFIG, database=DB_NAME)
     while True:
         print(f"Fetching page with size {page_size} at offset {offset}...")
         page = paginate_users(connection, page_size, offset)
