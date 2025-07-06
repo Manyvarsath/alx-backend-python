6cc9562d233b1668f2b6feb5159a70cf33a498ce
manyvarsath <avatar@LAPTOP-ACGOV0AD.localdomain>
2025-07-06 20:02:14 +0100

    Fixed 0-stream_users.py

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -1,9 +1,15 @@
 import mysql.connector
 
-def stream_users(connection):
-    if not connection:
-        return
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'varsath',
+    'password': 'varsath',
+    'port': 3306
+}
+DB_NAME = 'ALX_prodev'
 
+def stream_users():
+    connection = mysql.connector.connect(**DB_CONFIG, database=DB_NAME)
     try:
         cursor = connection.cursor(buffered=True)
         cursor.execute("SELECT user_id, name, email, age FROM user_data")
